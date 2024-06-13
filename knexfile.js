62e875413c0d7bfea0c640bf51c7136180e0d87d
Benjamin <bepelham6805@gmail.com>
2024-06-13 18:21:02 -0400

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,44 @@
 const path = require("path");
+
 require("dotenv").config();
-const { parse } = require('pg-connection-string');
- 
-// Environment setup
+
 const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
- 
-// Parse connection string and add SSL configuration for production
-const connectionConfig = parse(NODE_ENV === "production" ? PRODUCTION_DATABASE_URL : DEVELOPMENT_DATABASE_URL);
-if (NODE_ENV === "production") {
-  connectionConfig.ssl = {
-    rejectUnauthorized: false // Be cautious with this in production environments
-  };
-}
- 
+
+const ssl = { rejectUnauthorized: false };
+
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DEVELOPMENT_DATABASE_URL,
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl,
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
-    directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
- 
+
   production: {
     client: "postgresql",
-    connection: connectionConfig,
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl,
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "dr", "seeds"),
+      directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
- 
+
   test: {
     client: "sqlite3",
     connection: {
